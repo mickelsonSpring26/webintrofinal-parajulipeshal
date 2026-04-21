4b84d074b13587dd7277420c697b89a3c2a0f7f4
peshal.parajuli <peshal.parajuli@students.snow.edu>
2026-04-21 11:31:00 -0600

    added backend docker file

--- Backend/obj/Release/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Release/net10.0/Backend.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe93a4855ea8b88427ea398481a8d134fa897106")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
